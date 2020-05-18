1131fd379d3da62a7aefe2e31ffed18562c7be6b
ronewa <ronewamavhungu@gmail.com>
2020-05-18 13:03:41 +0200

    Updates

---
+++ precache-manifest.1d862b0d0c2491bd0ec0874d7a514ac9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15edbdcd3b380a0def004412c83bb817",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccefd15099d13396bf89",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88ac3622e96c083efa4c",
+    "url": "/static/js/2.55d24229.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55d24229.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccefd15099d13396bf89",
+    "url": "/static/js/main.b654260b.chunk.js"
+  },
+  {
+    "revision": "5855bf49084dc0464ab6",
+    "url": "/static/js/runtime-main.b71c37de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
